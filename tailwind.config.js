fd4fdb87f9f4d4c3a525aed185225e181634f196
John Mark Delima <delimajohnmark14@gmail.com>
2026-01-26 23:25:07 +0800

    Add Tailwind CSS configuration with custom theme settings

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        textMain: "#18181B",
+        cardBg: "#FFFFFF",
+        border: "#18181B",
+      },
+      fontFamily: {
+        logo: ["LilitaOne"],
+        body: ["Nunito-Regular"],
+        bodyBold: ["Nunito-Bold"],
+      },
+      borderRadius: {
+        'card': '32px',
+      },
+      borderWidth: {
+        '6': '6px',
+      },
+      boxShadow: {
+                100: "2px 2px 0px 0px rgb(0, 0, 0)",
+                200: "2px 2px 0px 2px rgb(0, 0, 0)",
+                300: "2px 2px 0px 2px rgb(238, 43, 105)",
+            },
+    },
+  },
+  plugins: [],
+}
