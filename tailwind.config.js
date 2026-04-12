e2e0930bc476cb4ade385852841ca4ae959e73ac
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-12 09:49:33 +0800

    Refactor tailwind.config.js to streamline color and font definitions, and add safelist for background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
+  safelist: [
+    {
+      pattern:
+        /bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)/,
+    },
+  ],
   theme: {
     extend: {
       screens: {
         xs: "360px",
       },
       colors: {
-        textMain: "#18181B",
         cardBg: "#FFFFFF",
-        border: "#18181B",
       },
       fontFamily: {
-        logo: ["RobotoCondensed_900Black", ...defaultTheme.fontFamily.sans],
-        body: defaultTheme.fontFamily.sans,
-        bodyBold: ["RobotoCondensed_700Bold", ...defaultTheme.fontFamily.sans],
-      },
-      borderRadius: {
-        'card': '32px',
-      },
-      borderWidth: {
-        '6': '6px',
+        logo: ["RobotoCondensed_900Black"],
+        body: ["sans-serif"],
+        bodyBold: ["RobotoCondensed_700Bold"],
       },
       boxShadow: {
-        100: "2px 2px 0px 0px rgb(0, 0, 0)",
-        200: "2px 2px 0px 2px rgb(0, 0, 0)",
-        300: "2px 2px 0px 2px rgb(238, 43, 105)",
-        card: "8px 8px 0px 0px #18181B",
-        "card-sm": "4px 4px 0px 0px #18181B"
+        200: "2px 2px 0px 0px #0a0a0a",
+        "card-sm": "2px 2px 0px 0px #0a0a0a"
       },
     },
   },
